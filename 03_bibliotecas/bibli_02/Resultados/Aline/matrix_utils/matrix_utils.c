0d3e581941c1471c9f1f8f25dbfb7c4719b60807
aline <ghidettialine@gmail.com>
2024-04-05 16:45:28 -0300

    odio

--- 03_bibliotecas/bibli_02/Resultados/Aline/matrix_utils/matrix_utils.c
+++ 03_bibliotecas/bibli_02/Resultados/Aline/matrix_utils/matrix_utils.c
@@ -2,20 +2,31 @@
 
 void matrix_read(int rows, int cols, int matrix[rows][cols]){
     int i, j;
-    for(i=0; i<cols; i++){
-        for(j=0; j<rows; j++){
-            scanf("%d", &matrix[rows][cols]);
+    for(i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
+            scanf("%d", &matrix[i][j]);
         }
     }
 }
 
 void matrix_print(int rows, int cols, int matrix[rows][cols]){
     int i, j;
-    for(i=0; i<cols; i++){
-        for(j=0; j<rows; j++){
-            printf("%d", matrix[rows][cols]);
+    for(i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
+            if(j==0){
+                printf("|");
+            }
+            printf("%d", matrix[i][j]);
+            if(j==cols-1){
+                printf("|");
+            }
+            else{
+                printf(" ");
+            }
         }
+        printf("\n");
     }
+    printf("\n");
 }
 
 int possible_matrix_sum(int rows1, int cols1, int rows2, int cols2){
@@ -28,28 +39,31 @@ int possible_matrix_sum(int rows1, int cols1, int rows2, int cols2){
 int possible_matrix_sub(int rows1, int cols1, int rows2, int cols2){
     if(rows1 == rows2 && cols1 == cols2){
         return 1;
-    }
+    } 
     return 0;
 }
 
 int possible_matrix_multiply(int cols1, int rows2){
-    if(cols1 == rows2);
+    if(cols1 == rows2){
+        return 1;
+    }
+    return 0;
 }
 
 void matrix_add(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2, int matrix2[rows2][cols2], int result[rows1][cols1]){
-    int i, j, k, l;
+    int i, j;
     for(i=0; i<rows1; i++){
         for(j=0; j<cols1; j++){
-            matrix1[i][j] = matrix1[i][j] + matrix2[i][j];
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
 }
 
 void matrix_sub(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2, int matrix2[rows2][cols2], int result[rows1][cols1]){
-    int i, j, k, l;
+    int i, j;
     for(i=0; i<rows1; i++){
         for(j=0; j<cols1; j++){
-            matrix1[i][j] = matrix1[i][j] - matrix2[i][j];
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
         }
     }
 }
@@ -69,6 +83,7 @@ void matrix_multiply(int rows1, int cols1, int matrix1[rows1][cols1], int rows2,
             }
     }
 }
+
 void transpose_matrix(int rows, int cols, int matrix[rows][cols], int result[cols][rows]){
     int i, j;
     for(i=0; i<rows; i++){
